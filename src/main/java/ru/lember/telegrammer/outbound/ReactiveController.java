eb3cf11a74a16ffeb5119c56798963171c978c11
Алексей Лемберский <alexlember@gmail.com>
2019-11-17 14:39:24 +0300

    7 - with-http profile

--- src/main/java/ru/lember/telegrammer/outbound/ReactiveController.java
+++ src/main/java/ru/lember/telegrammer/outbound/ReactiveController.java
@@ -1,18 +1,29 @@
 package ru.lember.telegrammer.outbound;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import reactor.core.publisher.Mono;
 import ru.lember.telegrammer.bot.BotHandler;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Profile("with-http")
 @RestController
 @RequestMapping
 public class ReactiveController {
 
     @Autowired
     private LongPollingBot botHandler;
+
+    @PostConstruct
+    private void postConstruct() {
+        log.info("initialized");
+    }
 
     @GetMapping("/isAvailable")
     public Mono<String> isAvailable() {
@@ -21,6 +32,8 @@ public class ReactiveController {
 
     @PostMapping("/testcmd")
     public Mono<String> cmd(@RequestBody ChatUpdate update) {
+
+        log.info("/testcmd: {}", update);
 
         if (botHandler instanceof BotHandler) {
             User user = new User(update.getUserId(), null, null, null, null, null);
